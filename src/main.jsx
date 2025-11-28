abf75c90a6a6acaa0e7d0c5616d8add81125495e
rickykam <rickykam829@gmail.com>
2025-11-29 00:20:07 +1100

    Change import from App.js to App.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.js' // 確保這裡指向您的主程式檔名
+import App from './App.jsx' 
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
